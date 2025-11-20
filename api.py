e1d272cfe12ff2ad72bcf39f385c735f150c5e90
Hasnat4763 <abulhasnat5737@gmail.com>
2025-11-20 11:27:58 +0600

    Changed handler and response so now it can serve html and json

--- api.py
+++ api.py
@@ -1,8 +1,51 @@
 from webob import Request, Response
+import parse
+import json
+
 
 class API:
+    def __init__(self):
+        self.routes = {}
+
     def __call__(self, env, start_response):
         request = Request(env)
-        response = Response()
-        response.text = "Hello, World!"
+        response = self.handle_request(request)
         return response(env, start_response)
+
+    def find_handler(self, reqpath, reqmethod):
+        for (path, method), handler in self.routes.items():
+            if method != reqmethod:
+                continue
+
+            parse_result = parse.parse(path, reqpath)
+            if parse_result is not None:
+                return handler, parse_result.named
+        return None, None
+
+    def handle_request(self, request):
+        handler, kwargs = self.find_handler(request.path, request.method)
+
+        if handler is None:
+            resp = Response()
+            resp.status_code = 404
+            resp.text = "404 Not Found"
+            return resp
+
+        result = handler(request, **(kwargs or {}))
+
+        if isinstance(result, Response):
+            return result
+
+        if isinstance(result, dict):
+            resp = Response(json_body=result)
+            return resp
+
+        resp = Response()
+        resp.text = str(result)
+        return resp
+
+    def route(self, path, method="GET"):
+        def wrapper(handler):
+            self.routes[(path, method)] = handler
+            return handler
+        return wrapper
--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 from api import API
+app = API()
+@app.route("/")
+def home(request):
+    return '''
+    <html>
+    <head>
+    <title>
+    Agartha
+    </title>
+    </head>
+    <body>
+    <p>
+    I am Agartha
+    </p>
+    </body>
+    </html>
+    '''
 
-def app(env, start_response):
-    api = API()
+
+@app.route("/about")
+def about(request):
+    return "AGARTHA WEB"
+    
+@app.route("/nigga")
+def nigga(request):
+    return f"{request.user_agent} you are a nigga"
+    
+@app.route("/sigma/{name}")
+def sigma(request, name):
+    return f"{name} Hi! im gonna hack you"
